879e4d4e589b334044adf017db97ee8431e387c7
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-02-07 04:38:26 -0600

    fixed button detection

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
+base_interval = 2500;
+long_interval = 2700000;
+active_interval = base_interval;
+
 setInterval(function() {
 	elements = document.getElementsByClassName("yt-spec-button-shape-next yt-spec-button-shape-next--text yt-spec-button-shape-next--call-to-action yt-spec-button-shape-next--size-m");
 	length = elements.length;
 	
 	for(var i = 0; i < length; ++i)
 		if(elements[i].ariaLabel === 'Yes') {
-			const style = window.getComputedStyle(elements[i]);
-			if(!(style.display === 'none' || style.visibility === 'hidden'))
-			{
-				elements[i].click();
-				console.log(Date.now(), "Clicked 'Yes' on", document.title);
-			}
+			elements[i].click();
+			console.log(Date.now(), "Clicked 'Yes' on", document.title);
+			
+			active_interval = long_interval;
 		}
-	
-}, 5000);
+}, active_interval);
 
 console.log("sapporo to new innocence");
