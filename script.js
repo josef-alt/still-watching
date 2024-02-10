61114b7d391922fe7852dbfe0bc729392c1111aa
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-02-10 08:52:01 -0600

    fixed repeated re-initialization of interval

--- script.js
+++ script.js
@@ -6,28 +6,29 @@ const base_interval = 2500;
 // be able to press the button while invisible, allowing
 // us to press it less often while never being interrupted.
 const long_interval = 2700000;
-var handle;
 
-// TODO break into two functions
-// once it switches long interval it acts like a setTimeout in a loop
-// ideally only set two intervals
 function stay_active() {
 	elements = document.getElementsByClassName("yt-spec-button-shape-next yt-spec-button-shape-next--text yt-spec-button-shape-next--call-to-action yt-spec-button-shape-next--size-m");
-	length = elements.length;
 	
-	for(var i = 0; i < length; ++i)
-		if(elements[i].ariaLabel === 'Yes') {
-			elements[i].click();
+	for(const element of elements) {
+		if(element.ariaLabel === 'Yes') {
+			element.click();
 			console.log(Date.now(), "Clicked 'Yes' on", document.title);
 			
 			// once the button appears we can increase the period
-			clearInterval(handle);
-			console.log('interval is', long_interval);
-			handle = setInterval(stay_active, long_interval);
+			if(active_interval === base_interval) {
+				clearInterval(handle);
+				active_interval = base_interval;
+				
+				console.log('interval is', active_interval);
+				handle = setInterval(stay_active, active_interval);
+			}
 		}
+	}
 }
 
-console.log('interval is', base_interval);
-handle = setInterval(stay_active, base_interval);
+let active_interval = base_interval;
+let handle = setInterval(stay_active, active_interval);
+console.log('interval is', active_interval);
 
 console.log("sapporo to new innocence");
