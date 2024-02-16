ba6910f4c447725cb8cc28c1735c86a008eda124
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-02-16 12:55:35 -0600

    fixed typo

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const base_interval = 2500;
 // once the button appears and has been pressed we should
 // be able to press the button while invisible, allowing
 // us to press it less often while never being interrupted.
-const long_interval = 2700000;
+const long_interval = 1800000;
 
 function stay_active() {
 	elements = document.getElementsByClassName("yt-spec-button-shape-next yt-spec-button-shape-next--text yt-spec-button-shape-next--call-to-action yt-spec-button-shape-next--size-m");
@@ -18,7 +18,7 @@ function stay_active() {
 			// once the button appears we can increase the period
 			if(active_interval === base_interval) {
 				clearInterval(handle);
-				active_interval = base_interval;
+				active_interval = long_interval;
 				
 				console.log('interval is', active_interval);
 				handle = setInterval(stay_active, active_interval);
